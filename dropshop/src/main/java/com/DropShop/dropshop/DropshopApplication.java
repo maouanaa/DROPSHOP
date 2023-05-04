c7a8b7f71a724e3fef9e92e7b71b34d749e41d27
maouanaa <sofienmw@gmail.com>
2023-05-04 14:47:20 +0100

    dropshopfirstversion

---
+++ dropshop/src/main/java/com/DropShop/dropshop/DropshopApplication.java
@@ -0,0 +1,13 @@
+package com.DropShop.dropshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropshopApplication.class, args);
+	}
+
+}
